9dc59279b8ee0a2f65830ac8e0b9d9ef7880aa3d
月 雪 <1072308083@qq.com>
2022-08-18 23:20:26 +0900

    init

--- Source/FPSGame/Private/FPSProjectile.cpp
+++ Source/FPSGame/Private/FPSProjectile.cpp
@@ -40,6 +40,9 @@ void AFPSProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPr
 	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
 	{
 		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
-		Destroy();
 	}
+
+	MakeNoise(1.0f, Instigator,GetActorLocation());
+	
+	//Destroy();
 }
